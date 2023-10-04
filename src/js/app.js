bf835357c3d4311ca5034f890897e5d48b111f98
Tatyana <lioness1741@mail.ru>
2023-10-04 16:10:15 +0500

    ready1

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,23 @@
 
 export default class Team {
-    constructor() {
-        this.members = new Set();
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('This hero already exists');
     }
-    add(character) {
-        if (this.members.has(character)) {
-          throw new Error('This hero already exists');
-        }
-        this.members.add(char);
-    }
-    addAll(...characters) {
-        characters.forEach((character) => {
-          this.members.add(character);
-        });
-      }
-      toArray() {
-        return [...this.members];
-      }
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => {
+      this.members.add(character);
+    });
+  }
+  
+  toArray() {
+    return [...this.members];
+  }
 }
